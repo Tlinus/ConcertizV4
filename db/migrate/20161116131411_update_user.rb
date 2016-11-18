b1b42d550299afcbb50100b0015e4b9d0d6b67eb
Tlinus <txl@hotmail.fr>
2016-11-18 16:13:31 +0100

    Revert "Table User Update"
    This reverts commit c14521eb184aca712406cb9269a74cd28f7f53fc.

--- db/migrate/20161116131411_update_user.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :utilisateurs, :username, :string
-    add_column :utilisateurs, :password, :string
-    add_column :utilisateurs, :email,    :string
-  end
-end
